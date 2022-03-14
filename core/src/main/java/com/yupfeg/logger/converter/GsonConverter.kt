cbd911d8359523c3510a9da67358cee891da9d55
yuPFeG <yupfeg1819@foxmail.com>
2022-03-14 18:46:30 +0800

    内聚Gson解析，移除单独的Gson解析工具类

--- core/src/main/java/com/yupfeg/logger/converter/GsonConverter.kt
+++ core/src/main/java/com/yupfeg/logger/converter/GsonConverter.kt
@@ -1,17 +1,23 @@
 package com.yupfeg.logger.converter
 
-import com.yupfeg.logger.converter.json.GsonUtils
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import java.lang.reflect.Type
 
 /**
  * gson的日志内容解析类
+ * - 用于其他对象内容类型的解析，如List、Map等
  * @author yuPFeG
  * @date 2021/03/31
  */
 class GsonConverter : JsonConverter{
 
-    override fun <T> fromJson(json: String, type: Type) : T = GsonUtils.fromJson(json, type)
+    private val gson: Gson by lazy(LazyThreadSafetyMode.SYNCHRONIZED){
+        GsonBuilder().setLenient().create()
+    }
 
-    override fun toJson(data: Any): String = GsonUtils.toJson(data)
+    override fun <T> fromJson(json: String, type: Type) : T = gson.fromJson(json, type)
+
+    override fun toJson(data: Any): String = gson.toJson(data)
 
 }
