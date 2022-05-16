0464231172e16079e45f0695475ad224a8e00f53
yuPFeG <yupfeg1819@foxmail.com>
2022-05-16 11:46:55 +0800

    feat : 1. 优化json格式化解析string类型逻辑; 2. 整合统一是否开启json格式化两种情况的处理逻辑

--- core/src/main/java/com/yupfeg/logger/handle/StringPrintHandler.kt
+++ core/src/main/java/com/yupfeg/logger/handle/StringPrintHandler.kt
@@ -1,9 +1,8 @@
 package com.yupfeg.logger.handle
 
-import com.yupfeg.logger.converter.JsonConverter
+import com.yupfeg.logger.LogPrintRequest
 import com.yupfeg.logger.converter.formatJSONString
 import com.yupfeg.logger.formatter.Formatter
-import com.yupfeg.logger.LogPrintRequest
 import com.yupfeg.logger.handle.parse.Parsable
 import org.json.JSONArray
 import org.json.JSONException
@@ -20,13 +19,22 @@ internal class StringPrintHandler : BasePrintHandler(), Parsable<String> {
         return trimLogContent(request.logContent).isNotEmpty()
     }
 
+    override fun formatLogContentOnlyWrap(
+        logFormatter: Formatter,
+        request: LogPrintRequest
+    ): String = formatLogContent(logFormatter, request)
+
     override fun formatLogContent(logFormatter: Formatter, request: LogPrintRequest): String {
         val logContentFormat = getLogFormatContentWrap(logFormatter)
         val trimLog = trimLogContent(request.logContent)
-        val parseContent = parse2String(trimLog,logFormatter,globalJsonConverter)
+        val parseContent = parse2String(trimLog,logFormatter)
         return String.format(logContentFormat, parseContent)
     }
 
+    /**
+     * 移除日志字符串内容里前导与后缀的多余空格字符串
+     * @param content 原始日志内容字符串
+     * */
     private fun trimLogContent(content: Any) : String{
         return if (content is StringBuilder || content is StringBuffer){
             content.toString().trim { it <= ' ' }
@@ -37,35 +45,31 @@ internal class StringPrintHandler : BasePrintHandler(), Parsable<String> {
         }
     }
 
-    override fun parse2String(
-        content: String,
-        formatter: Formatter,
-        jsonConverter: JsonConverter
-    ) : String{
+    override fun parse2String(content: String, formatter: Formatter) : String{
         return try {
             when {
                 //json对象
                 content.startsWith("{") -> {
                     val jsonObject = JSONObject(content)
                     jsonObject.formatJSONString().run {
-                        replace("\n", "\n${formatter.left}")
+                        replace("\n", "${Formatter.BR}${formatter.left}")
                     }
                 }
                 //json数组
                 content.startsWith("[") -> {
                     val jsonArray = JSONArray(content)
                     jsonArray.formatJSONString().run {
-                        replace("\n", "\n${formatter.left}")
+                        replace("\n", "${Formatter.BR}${formatter.left}")
                     }
                 }
                 // 普通的字符串
                 else -> {
-                    content.replace("\n", "\n${formatter.left}")
+                    content.replace("\n", "${Formatter.BR}${formatter.left}")
                 }
             }
         } catch (e: JSONException) {
             //特殊情况，解析失败，按普通字符串处理
-            content.replace("\n", "\n${formatter.left}")
+            content.replace("\n", "${Formatter.BR}${formatter.left}")
         }
     }
 
