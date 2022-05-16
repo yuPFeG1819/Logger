555b4aa3c771162c0566b50babb99fe2ceb152f9
yuPFeG <yupfeg1819@foxmail.com>
2022-05-16 11:00:24 +0800

    feat : 1. 废弃top-level的logger配置方法 2. 替换top-level的简化全局日志输出方法实现

--- core/src/main/java/com/yupfeg/logger/ext/LoggerExt.kt
+++ core/src/main/java/com/yupfeg/logger/ext/LoggerExt.kt
@@ -7,27 +7,24 @@ import com.yupfeg.logger.LoggerConfig
  * 使用kotlin DSL方式，配置Logger参数
  * @param init dsl方式配置
  * */
-fun setDslLoggerConfig(init : LoggerConfig.()->Unit) : Logger {
-    return LoggerConfig().let {
-        it.init()
-        performConfigLogger(it)
-    }
+@Deprecated(
+    message = "已统合到Logger类的静态方法",
+    replaceWith = ReplaceWith(
+        expression = "Logger.prepare(init)",
+        imports = ["com.yupfeg.logger.Logger"]
+    )
+)
+fun setDslLoggerConfig(init : LoggerConfig.()->Unit) : Logger{
+    return Logger.prepare(init)
 }
 
-/**
- * 执行配置Logger操作
- * @param config
- * */
-private fun performConfigLogger(config: LoggerConfig) : Logger {
-    return Logger.prepare(config)
-}
 /**
  * 输出verbose等级的日志
  * * 使用全局的日志tag
  * @param msg 日志内容
  */
 @Suppress("SpellCheckingInspection", "unused")
-fun logv(msg : Any) = Logger.v(msg = msg)
+fun logv(msg : Any) = Logger.GLOBAL.v(msg = msg)
 
 /**
  * 输出debug等级的日志
@@ -35,7 +32,7 @@ fun logv(msg : Any) = Logger.v(msg = msg)
  * @param msg 日志内容
  */
 @Suppress("SpellCheckingInspection", "unused")
-fun logv(tag : String,msg: Any) = Logger.v(tag, msg)
+fun logv(tag : String,msg: Any) = Logger.GLOBAL.v(tag, msg)
 
 /**
  * 输出debug等级的日志
@@ -43,7 +40,7 @@ fun logv(tag : String,msg: Any) = Logger.v(tag, msg)
  * @param msg 日志内容
  */
 @Suppress("SpellCheckingInspection", "unused")
-fun logd(msg : Any) = Logger.d(msg = msg)
+fun logd(msg : Any) = Logger.GLOBAL.d(msg = msg)
 
 /**
  * 输出debug等级的日志
@@ -51,7 +48,7 @@ fun logd(msg : Any) = Logger.d(msg = msg)
  * @param msg 日志内容
  */
 @Suppress("SpellCheckingInspection", "unused")
-fun logd(tag : String, msg : Any?) = Logger.d(tag,msg)
+fun logd(tag : String, msg : Any?) = Logger.GLOBAL.d(tag,msg)
 
 /**
  * 输出info等级的日志
@@ -59,7 +56,7 @@ fun logd(tag : String, msg : Any?) = Logger.d(tag,msg)
  * @param msg 日志内容
  */
 @Suppress("SpellCheckingInspection", "unused")
-fun logi(msg : Any?) = Logger.i(msg = msg)
+fun logi(msg : Any?) = Logger.GLOBAL.i(msg = msg)
 
 /**
  * 输出info等级的日志
@@ -67,7 +64,7 @@ fun logi(msg : Any?) = Logger.i(msg = msg)
  * @param msg 日志内容
  */
 @Suppress("SpellCheckingInspection", "unused")
-fun logi(tag : String, msg : Any) = Logger.i(tag,msg)
+fun logi(tag : String, msg : Any) = Logger.GLOBAL.i(tag,msg)
 
 /**
  * 输出waring等级的日志
@@ -75,7 +72,7 @@ fun logi(tag : String, msg : Any) = Logger.i(tag,msg)
  * @param msg 日志内容
  */
 @Suppress("SpellCheckingInspection", "unused")
-fun logw(msg: Any?) = Logger.w(msg = msg)
+fun logw(msg: Any?) = Logger.GLOBAL.w(msg = msg)
 
 /**
  * 输出waring等级的日志
@@ -83,7 +80,7 @@ fun logw(msg: Any?) = Logger.w(msg = msg)
  * @param msg 日志内容
  */
 @Suppress("SpellCheckingInspection", "unused")
-fun logw(tag: String,msg: Any?) = Logger.w(tag,msg)
+fun logw(tag: String,msg: Any?) = Logger.GLOBAL.w(tag,msg)
 
 /**
  * 输出error等级的日志
@@ -91,7 +88,7 @@ fun logw(tag: String,msg: Any?) = Logger.w(tag,msg)
  * @param msg 日志内容
  */
 @Suppress("SpellCheckingInspection", "unused")
-fun loge(msg: Any?) = Logger.e(msg = msg)
+fun loge(msg: Any?) = Logger.GLOBAL.e(msg = msg)
 
 /**
  * 输出error等级的日志
@@ -99,4 +96,4 @@ fun loge(msg: Any?) = Logger.e(msg = msg)
  * @param msg 日志内容
  */
 @Suppress("SpellCheckingInspection", "unused")
-fun loge(tag : String,msg : Any?) = Logger.e(tag,msg)
+fun loge(tag : String,msg : Any?) = Logger.GLOBAL.e(tag,msg)
