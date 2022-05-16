014c0628a38e0bb2881c333514ead5a879d0f43c
yuPFeG <yupfeg1819@foxmail.com>
2022-05-16 11:28:17 +0800

    feat : 优化json格式化解析bundle类型逻辑

--- core/src/main/java/com/yupfeg/logger/handle/BundlePrintHandler.kt
+++ core/src/main/java/com/yupfeg/logger/handle/BundlePrintHandler.kt
@@ -2,11 +2,11 @@ package com.yupfeg.logger.handle
 
 import android.os.Bundle
 import com.yupfeg.logger.LogPrintRequest
-import com.yupfeg.logger.converter.JsonConverter
 import com.yupfeg.logger.converter.formatJSONString
 import com.yupfeg.logger.converter.parseToJSONObject
 import com.yupfeg.logger.formatter.Formatter
 import com.yupfeg.logger.handle.parse.Parsable
+import org.json.JSONException
 
 /**
  * [Bundle]类型的日志输出处理类
@@ -21,25 +21,18 @@ internal class BundlePrintHandler : BasePrintHandler(), Parsable<Bundle> {
 
     override fun formatLogContent(logFormatter: Formatter, request : LogPrintRequest): String {
         val logContentFormat = getLogFormatContentWrap(logFormatter)
-        val logContent = parse2String(
-            request.logContent as Bundle,logFormatter,globalJsonConverter
-        )
+        val logContent = parse2String(request.logContent as Bundle,logFormatter)
         return String.format(logContentFormat,logContent)
     }
 
-    override fun parse2String(
-        content: Bundle,
-        formatter: Formatter,
-        jsonConverter: JsonConverter
-    ): String {
+    override fun parse2String(content: Bundle, formatter: Formatter): String {
         val header = "${content.javaClass}${Formatter.BR}${formatter.left}"
         val logContent = try {
-            content
-                .parseToJSONObject(jsonConverter)
+            content.parseToJSONObject(globalJsonConverter)
                 .formatJSONString()
-                .replace("\n", "\n${formatter.left}")
-        }catch (e : Exception){
-            content.toString().replace("\n", "\n${formatter.left}")
+                .replace("\n", "${Formatter.BR}${formatter.left}")
+        }catch (e : JSONException){
+            content.toString().replace("\n", "${Formatter.BR}${formatter.left}")
         }
         return "$header$logContent"
     }
