7fdf5d2e926fcca7e690c4a80dfd104f345a7099
yuPFeG <yupfeg1819@foxmail.com>
2022-05-16 10:38:30 +0800

    feat : 1. 修改对外公开单例类为壳对象类，提供一个壳对象作用域范围内的tag； 2. 整合dsl配置方法到壳对象类的静态方法； 3. 新增未执行初始化不进行日志输出操作，并手动输出警告日志，避免未初始化日志库而不好排查问题

--- core/src/main/java/com/yupfeg/logger/Logger.kt
+++ core/src/main/java/com/yupfeg/logger/Logger.kt
@@ -1,65 +1,107 @@
 package com.yupfeg.logger
 
+import android.util.Log
+
 /**
- * 日志库的对外公开类，全局单例
+ * 日志库的对外公开类
+ * 壳对象，仅用于设置专属的局部日志输出Tag。
  * @author yuPFeG
  * @date 2020/12/31
  */
-object Logger {
+class Logger private constructor(private val globalTag : String?) {
 
-    private var mLogConfig : LoggerConfig = LoggerConfig()
-    @Volatile
-    private var mLoggerCore : LoggerCore? = null
+    companion object{
+        private var logConfig : LoggerConfig?= null
+        @Volatile
+        private var loggerCore : LoggerCore? = null
 
-    //<editor-fold desc="配置方法">
+        /**
+         * 全局的日志类，简化日志输出操作
+         * */
+        internal val GLOBAL = Logger(null)
+
+        /**
+         * 是否已进行初始化
+         * */
+        private var isPrepared : Boolean = false
+
+        /**
+         * 以kotlin-dsl的方式，初始化日志库配置
+         * @param init dsl配置方法
+         * @return Lo
+         * */
+        fun prepare(init : LoggerConfig.()->Unit) : Logger{
+            return LoggerConfig().let {config->
+                config.init()
+                prepare(config)
+            }
+        }
+        /**
+         * 准备日志库配置
+         * - 必须在执行[prepare]后，并配置日志输出目标才能才能输出日志
+         * @param config 配置方法类
+         * */
+        @JvmStatic
+        fun prepare(config: LoggerConfig) : Logger{
+            require(!isPrepared){ "cant replay prepare Logger，only config once" }
+            logConfig = config
+            return GLOBAL
+        }
+
+        /**
+         * 创建日志输出壳对象
+         * @param clazz 以类名`simpleName`作为局部日志tag名称
+         * */
+        fun create(clazz : Class<*>) : Logger = Logger(globalTag = clazz.simpleName)
+
+        /**
+         * 创建日志输出壳对象
+         * @param
+         * */
+        fun create(tag : String) : Logger = Logger(globalTag = tag)
+
+    }
 
     /**
-     * 准备日志库配置
-     * @param config 配置方法类
+     * 全局控制当前壳对象是否开启日志输出
      * */
-    @JvmStatic
-    fun prepare(config: LoggerConfig) : Logger{
-        mLoggerCore?.also {
-            throw IllegalArgumentException("cant replay prepare Logger，only config once")
-        }
-        mLogConfig = config
+    private var isEnable = true
+
+    /**
+     * 关闭壳对象的日志输出
+     * */
+    fun silence() : Logger{
+        this.isEnable = false
         return this
     }
 
-    // </editor-fold>
 
     //<editor-fold desc="日志输出方法">
 
     @Suppress("unused")
-    @JvmStatic
-    @JvmOverloads
     fun v(tag : String?= null,msg: Any?) = performPrintLog(LoggerLevel.VERBOSE,tag,msg)
 
     @Suppress("unused")
-    @JvmStatic
-    @JvmOverloads
     fun i(tag: String?= null, msg : Any?) = performPrintLog(LoggerLevel.INFO,tag,msg)
 
     @Suppress("unused")
-    @JvmStatic
-    @JvmOverloads
     fun d(tag: String? = null, msg: Any?) = performPrintLog(LoggerLevel.DEBUG,tag, msg)
 
     @Suppress("unused")
-    @JvmStatic
-    @JvmOverloads
     fun w(tag: String? = null, msg: Any?) = performPrintLog(LoggerLevel.WARN,tag, msg)
 
 
     @Suppress("unused")
-    @JvmStatic
-    @JvmOverloads
     fun e(tag: String? = null, msg: Any?) = performPrintLog(LoggerLevel.ERROR,tag, msg)
+
+    // </editor-fold>
 
     /**
      * 执行输出日志操作
+     * - 日志输出的实际入口
      * @param level 日志等级
-     * @param tag 日志tag
+     * @param tag 日志tag，如果为null，则使用壳对象的局部tag，
+     * 如果局部tag也为null，则使用配置的全局tag。
      * @param message 日志内容
      * */
     private fun performPrintLog(
@@ -67,16 +109,24 @@ object Logger {
         tag : String?,
         message : Any?
     ){
+        if (!isPrepared) {
+            //避免日志未配置输出类
+            Log.w(tag?:globalTag, "you should prepare logger config before print log" +
+                    " \n ${message?.toString() ?: ""}")
+            return
+        }
+        if (!isEnable) return
         message?:return
-        getNewCoreInstance().printLogWithHandlerChain(level,tag, message)
+        getNewCoreInstance().printLogWithHandlerChain(level,tag?:globalTag, message)
     }
 
+    /**
+     * 获取日志输出核心类实例
+     */
     private fun getNewCoreInstance() : LoggerCore{
-        return mLoggerCore ?: synchronized(Logger::class.java){
-            mLoggerCore ?: LoggerCore(mLogConfig).also { mLoggerCore = it }
+        return loggerCore ?: synchronized(Logger::class.java){
+            loggerCore ?: LoggerCore(logConfig?:LoggerConfig()).also { loggerCore = it }
         }
     }
-
-    //</editor-fold desc="日志输出方法">
 
 }
